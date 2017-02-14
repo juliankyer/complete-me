4deb3446892c3d0de84387a15e13844fce471d37
Julian Kyer <julian.kyer@gmail.com>
2017-02-13 17:10:15 -0700

    Add basic HTML in case I make it that far

--- scripts/CompleteMe.js
+++ scripts/CompleteMe.js
@@ -1,3 +1,5 @@
+import Node from '../scripts/Node';
+
 class CompleteMe {
   constructor() {
     
---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+$('.save-button').on('click', function(e) {
+  e.preventDefault();
+  console.log('boom');
+  //search trie on click here
+});
